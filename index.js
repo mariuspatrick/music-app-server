6c0598a2fcb78df2773dd64cd4cb87694ab91a7d
mariuspatrick <marius.patrick72@gmail.com>
2020-01-13 12:08:27 +0100

    Fixing tables

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const spotifyRouter = require("./spotify");
 const signup = require("./signup/router");
 const { router: loginRouter } = require("./login/router");
-const Song = require("./tracks/model");
-const songRouter = require("./tracks/router");
 const routerUserPlaylist = require("./user_playlist/router");
 
 const cors = require("cors");
@@ -18,7 +16,6 @@ app.use(jsonParser);
 app.use(spotifyRouter);
 app.use(signup);
 app.use(loginRouter);
-app.use(songRouter);
 app.use(routerUserPlaylist);
 
 app.listen(port, console.log(`Listening on port: ${port}`));
